fdf0251248e8f7496f2f4c8765012e24f11ba7b3
alppacat <lorena.pratts@gmail.com>
2018-07-17 15:27:25 -0500

    Crea el archivo js que contiene la funcionalidad que crea un nuevo usuario

--- src/js/newuser.js
+++ src/js/newuser.js
@@ -11,36 +11,40 @@
   firebase.initializeApp(config);
 
   // Get elements
+  const txtName = document.getElementById('txt-name');
   const txtEmail = document.getElementById('txt-email');
   const txtPassword = document.getElementById('txt-password');
-  const btnLogin = document.getElementById('btn-login');
+  const btnSignUp = document.getElementById('btn-sign-up');
 
-  // Add login event
-  btnLogin.addEventListener('click', event => {
+  // Add signup event
+  btnSignUp.addEventListener('click', event => {
     // Get email and password
     const email = txtEmail.value;
     const password = txtPassword.value;
     const auth = firebase.auth();
     // Sign in
-    const promise = auth.signInWithEmailAndPassword(email, password);
-    promise.catch(event => alert(event.message));
+    const promise = auth.createUserWithEmailAndPassword(email, password);
+    promise
+      .catch(event =>
+        alert(event.message));
   });
 
   // Add a realtime listener
+
   firebase.auth().onAuthStateChanged(firebaseUser => {
     if (firebaseUser) {
       console.log(firebaseUser);
-      window.location.assign('../views/home.html');
+      window.location.assign('home.html');
       let user = firebase.auth().currentUser;
       if (user !== null) {
-        // let emailId = user.email;
         user.updateProfile({
           displayName: txtName.value
         });
-        document.getElementById('user-paragraph').innerHTML = `Bienvenidx ${user.displayName}`;
+        document.getElementById('user-paragraph').innerHTML = ` Bienvenido ${user.displayName};`;
       }
     } else {
       console.log('not logged in');
     }
   });
 }());
+
