13df2f1a5d0bdab4e8b337029d341eb5f7c64592
marelywoody <ros_ely_arc@hotmail.com>
2018-07-17 20:11:58 -0500

    cambiar de html

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 let register = document.getElementById('register');
 let enter = document.getElementById('enter');
-
+// Evento donde se registran los usuarios código de firebase
 register.addEventListener('click', event => {
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
@@ -11,8 +11,11 @@ register.addEventListener('click', event => {
         var errorMessage = error.message;
         // ...
       });
+     if (email != null) {
+      window.open('../src/views/view1.html','_self');
+     }
 });
-
+// Evento de iniciar sesion
 enter.addEventListener('click', event => {
     let enterEmail = document.getElementById('enter-email').value;
     let enterPassword = document.getElementById('enter-password').value;
@@ -24,13 +27,14 @@ enter.addEventListener('click', event => {
         // ...
       });
  });
-
+// funsión para ver si un usuario esta activo o no
  const monitor = () => {
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
           // User is signed in.
           var displayName = user.displayName;
           var email = user.email;
+          window.open('../src/views/view1.html','_self');
           var emailVerified = user.emailVerified;
           var photoURL = user.photoURL;
           var isAnonymous = user.isAnonymous;
@@ -44,4 +48,3 @@ enter.addEventListener('click', event => {
       });
  }; 
  monitor();
- 
