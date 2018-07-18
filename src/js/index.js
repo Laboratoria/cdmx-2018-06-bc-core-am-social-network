626a3a9c35c0d1c4759edfa984cf38448f520220
anameneses <anushkiss@hotmail.com>
2018-07-17 20:45:22 -0500

    cambios viejos

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,17 @@
-(function() {
+(function(){
+    const config = {
+    apiKey: "<API_KEY>",
+    authDomain: "<PROJECT_ID>.firebaseapp.com",
+    databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+    storageBucket: "<BUCKET>.appspot.com",
+    messagingSenderId: "<SENDER_ID>",
+  };
+  firebase.initializeApp(config);
+const correo = document.getElementById("email");
+const contra = document.getElementById("pasword");
+const entrar = documente.getElementById("submit");
+const registro = document.getElementById("registro");
 
-        const config = {
 
-        }
-    }
+
+    }());
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,1 +1,11 @@
 // Ejecuta tu test 
+const {assert} = require('chai');
+const{inputNotEmpty} = require ('../src/js/index.js');
+
+describe('inputNotEmpty()', ()=>{
+    it ('deberia ser una funcion', ()=>{
+        assert.equal(typeof inputNotEmpty, 'function')
+    })
+    it('deberia return true en caso de el input no este vacio')
+})
+it  
