0668451638eda153f626dd18c4509a65352340f4
DianaCarrillo <39560661+DianaCarrillo@users.noreply.github.com>
2018-07-17 19:55:32 -0500

    agregando 2 html y funcionalidad de login y sign up

--- src/js/index.js
+++ src/js/index.js
@@ -1,38 +1,67 @@
  (function () {
-   // Initialize Firebase
-   const config = {
-     apiKey: "AIzaSyBJVkG4r0b5pauCPkgJ7lF8TNGwqlNE7Z0",
-     authDomain: "red-social-ea5c4.firebaseapp.com",
-     databaseURL: "https://red-social-ea5c4.firebaseio.com",
-     projectId: "red-social-ea5c4",
-     storageBucket: "red-social-ea5c4.appspot.com",
-     messagingSenderId: "596516685843"
-   };
-   firebase.initializeApp(config);
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCbW5lXCLbbrY6EkFROvAukVs8herq8G-Y",
+    authDomain: "social-network-5b9ef.firebaseapp.com",
+    databaseURL: "https://social-network-5b9ef.firebaseio.com",
+    projectId: "social-network-5b9ef",
+    storageBucket: "",
+    messagingSenderId: "1074635944561"
+  };
+  firebase.initializeApp(config);
+
 
    // Get elements
-   const email = document.getElementById('email');
-   const password = document.getElementById('password');
-   const logIn = document.getElementById('btn-login');
-   const signUp = document.getElementById('btn-signup');
+   let mail = document.getElementById('email');
+   let password = document.getElementById('password');
+   const logIn = document.getElementById('log-in');
+   const signUp = document.getElementById('sign-up');
+   const username = document.getElementById('first_name');
+   const lastname = document.getElementById('last_name');
+
 
    // agregar evento click al botón de log in
    logIn.addEventListener('click', e => {
+    event.preventDefault();
      // Obtener valores de email y password
-     const emailValue = email.value;
-     const passwordValue = password.value;
+     let emailValue = mail.value;
+     let passwordValue = password.value;
      const auth = firebase.auth();
-
-     // para iniciar sesión (log in)
+     let usuario ={
+      name: username.value,
+      apellido: lastname.value,
+      email: email.value
+     }
+     localStorage.setItem("key", JSON.stringify(usuario));
+     console.log(usuario);
+// para iniciar sesión (log in)
+window.homeNetwork.mostrar(usuario);
      const promise = auth.signInWithEmailAndPassword(emailValue, passwordValue);
      promise.catch(e => console.log(e.message));
+    //  datosUsuario(emailValue, passwordValue);
    });
+
+  
+    //  let datosUsuario = (emailValue,passwordValue) => {
+    //   let usuario ={
+    //    name: username.value,
+    //    apellido: lastname.value,
+    //    email: email.value
+    //   }
+     
+    //  }
+   
+   
+
+  
 
    //botón de registrarse (sign up)
 
    signUp.addEventListener('click', e => {
-     const emailValue = email.value;
-     const passwordValue = password.value;
+    event.preventDefault();
+
+     let emailValue = email.value;
+     let passwordValue = password.value;
      const auth = firebase.auth();
 
      // para registrarse ( sign in)
