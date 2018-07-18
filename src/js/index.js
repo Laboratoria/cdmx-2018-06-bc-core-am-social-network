6f88cf0adec4f77ef0f9fac6abc550ab78ba46c7
marelywoody <ros_ely_arc@hotmail.com>
2018-07-18 04:25:47 -0500

    window problema reparado

--- src/js/index.js
+++ src/js/index.js
@@ -1,31 +1,35 @@
 let register = document.getElementById('register');
 let enter = document.getElementById('enter');
+const wintowTras = () =>{
+  window.open('../src/views/view1.html','_self');
+}
 // Evento donde se registran los usuarios código de firebase
 register.addEventListener('click', event => {
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-    .catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-      });
-     if (email != null) {
-      window.open('../src/views/view1.html','_self');
+     if (email != '' && password != '') {
+      firebase.auth().createUserWithEmailAndPassword(email, password)
+      .catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // ...
+        });
      }
 });
 // Evento de iniciar sesion
 enter.addEventListener('click', event => {
     let enterEmail = document.getElementById('enter-email').value;
     let enterPassword = document.getElementById('enter-password').value;
-    firebase.auth().signInWithEmailAndPassword(enterEmail, enterPassword)
-    .catch(function(error) {
+    if (enterEmail != '' && enterPassword != '') {
+      firebase.auth().signInWithEmailAndPassword(enterEmail, enterPassword)
+      .catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
         // ...
       });
+    }
  });
 // funsión para ver si un usuario esta activo o no
  const monitor = () => {
@@ -34,7 +38,7 @@ enter.addEventListener('click', event => {
           // User is signed in.
           var displayName = user.displayName;
           var email = user.email;
-          window.open('../src/views/view1.html','_self');
+          wintowTras();
           var emailVerified = user.emailVerified;
           var photoURL = user.photoURL;
           var isAnonymous = user.isAnonymous;
