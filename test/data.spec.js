e560654349ccb98687c009af3a9a79256ac2c337
anameneses <anushkiss@hotmail.com>
2018-07-19 11:28:17 -0500

    subiendo readme ux, alerts de buttons

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,16 +1,4 @@
 // Ejecuta tu test 
-<<<<<<< HEAD
-const {assert} = require('chai');
-const{inputNotEmpty} = require ('../src/js/index.js');
-
-describe('inputNotEmpty()', ()=>{
-    it ('deberia ser una funcion', ()=>{
-        assert.equal(typeof inputNotEmpty, 'function')
-    })
-    it('deberia return true en caso de el input no este vacio')
-})
-it  
-=======
 const { assert } = require('chai');
 const InputNotEmpty = require('../src/js/index.js');
 
@@ -20,4 +8,3 @@ it('deberia...', () => {
 })
 it('Debería retornar un true en caso de que el input no este vacio')
 })
->>>>>>> upstream/master
