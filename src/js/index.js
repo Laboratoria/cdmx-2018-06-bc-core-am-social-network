5fc9a3ca1a5c4de44653150e3c15f7a6f653b47e
alppacat <lorena.pratts@gmail.com>
2018-07-20 00:21:20 -0500

    Sube cambio de archivos js: se mueve inicializacion de firebase a un archivo js aparte

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,4 @@
 (function() {
-  // Initialize Firebase
-  var config = {
-    apiKey: 'AIzaSyC20SbyxB9RKJgcfTvfYuhJKxuuxh0RQBQ',
-    authDomain: 'prueba-firebase-ef04b.firebaseapp.com',
-    databaseURL: 'https://prueba-firebase-ef04b.firebaseio.com',
-    projectId: 'prueba-firebase-ef04b',
-    storageBucket: 'prueba-firebase-ef04b.appspot.com',
-    messagingSenderId: '499846254860'
-  };
-  firebase.initializeApp(config);
-
   // Get elements
   const txtEmail = document.getElementById('txt-email');
   const txtPassword = document.getElementById('txt-password');
