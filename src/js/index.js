82cf2d07ff1c6d422a184e0985472cd179855db5
gloryarz <gloryarz@gmail.com>
2018-07-20 08:21:07 -0500

    Pruebas

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,19 @@
-const color = document.getElementById('color');
-
+let app_fireBase = {};
+(function(){
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyCGT48tRRTrbUJvfWb3PvKZfSgISreF1S0",
+    authDomain: "red-social-96a75.firebaseapp.com",
+    databaseURL: "https://red-social-96a75.firebaseio.com",
+    projectId: "red-social-96a75",
+    storageBucket: "",
+    messagingSenderId: "775319261849"
+  };
+  firebase.initializeApp(config);
+  app_fireBase = firebase;
+})()
+  
+/*
 // Get HTML elements for Login
 const email = document.getElementById('mail');
 const password = document.getElementById('password');
@@ -37,3 +51,30 @@ firebase.auth().onAuthStateChanged(firebaseUser => {
         
     }
 })
+*/
+let mainApp = {};
+(function(){
+    let firebase = app_fireBase;
+let uid = null;
+
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+        // User is signed in.
+            uid = user.uid
+        } else {
+            uid = null;
+            window.location.replace('https://github.com/gloryarz/cdmx-2018-06-bc-core-am-social-network');
+        }
+    });
+
+    function logOut (){
+        firebase.auth().signOut();
+    }
+
+    mainApp.logOut = logOut;
+
+})()
+
+
+
+// document.getElementById('logout').addEventListener('click', )
