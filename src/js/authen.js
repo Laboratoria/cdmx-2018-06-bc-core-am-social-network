bf2c3d9165a6ae8cbb6b53fb3e2301e339229736
marelywoody <ros_ely_arc@hotmail.com>
2018-07-22 16:16:56 -0500

    cuenta google

--- src/js/authen.js
+++ src/js/authen.js
@@ -7,3 +7,13 @@
 //     let user = new firebase.auth.GoogleAuthProvider();
 //     authen.signInWithPopup(user);
 // })
+let loginGoogle = document.getElementById('google-user');
+
+loginGoogle.addEventListener('click', event => {
+  let provider = new firebase.auth.GoogleAuthProvider();
+  provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+  firebase.auth()
+  .signInWithPopup(provider)
+  .then(result => {
+  })
+});    
--- src/js/database.js
+++ src/js/database.js
@@ -1,2 +1,14 @@
 // Accedo al servicio de la base de datos
 // let database = firebase.database();
+const wintowTras = () =>{
+  window.open('../index.html','_self');
+}
+let close = document.getElementById('close');
+close.addEventListener('click', event => { 
+  firebase.auth().signOut().then(function() {
+    // Sign-out successful.
+    wintowTras();
+  }).catch(function(error) {
+    // An error happened.
+  });
+});
