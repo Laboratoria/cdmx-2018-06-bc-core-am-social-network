07c037e987c424880566afe3a14b3d7567b1f55b
luz <adrivioleta@hotmail.com>
2018-07-22 23:04:29 -0500

    Mejorando vista e inicio de sesion con facebook y google

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,18 @@
-let texto = document.getElementById('textInput');
+let text = document.getElementById('comentario');
 let boton = document.getElementById('botonP');
-let comentario = document.getElementById('comentario');
+let newPost = document.getElementById('container');
+console.log(text);
 
-boton.addEventListener('click', evento =>{
-  newPost.innerHTML = textInput.value;
-});
+boton.addEventListener("click",event =>{
+  console.log('holi');
+  let texto = text.value;
+  console.log(texto);
+  let divPost = '';
+  divPost += `<div class="card">
+                    <div class="contenidoC">
+                        <p>${texto}</p>
+                    </div>
+                </div>`
+                console.log(divPost);
+                container.innerHTML = divPost;
+})
