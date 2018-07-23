335075c9904ac64fc2e6ba9575da51f3cf712b0a
[Copelia] <iamcorallium@gmail.com>
2018-07-23 09:08:22 -0500

    Subiendo archivos de código para publicar

---
+++ Publicaciones prueba/js/main.js
@@ -0,0 +1,8 @@
+// Firebase
+// Conexiones
+ var db = firebase.database();
+ var storage  = firebase.storage();
+//
+// // Referencias ...Rama?
+ var housingRef = db.ref('housing/');
+ var storageRef = storage.ref();
