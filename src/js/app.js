6219f299a4bfd7639a3af9430864f6d2d4a3ed71
frufrula <l.tgessner@gmail.com>
2018-07-23 09:13:03 -0500

    message persistency ok/message realtime actualization ok

--- src/js/app.js
+++ src/js/app.js
@@ -5,15 +5,61 @@ firebase.initializeApp({
   projectId: "red-social-237f9"
 });
 
-// Initialize Cloud Firestore through Firebase
-var db = firebase.firestore();
+// Con esto se inicializa Nube de firestore a través de Firebase
+let db = firebase.firestore();
 
 
-let text = document.getElementById('comentario');
 let boton = document.getElementById('botonP');
-let newPost = document.getElementById('container');
-console.log(text);
+boton.addEventListener("click", event => {
+  // console.log('holi');
+  let text = document.getElementById('comentario');
+  // console.log(text);
 
+  let texto = text.value;
+//   construir nuevo objeto de fecha y convertirlo a un string UTC para pintarlo de manera estandard
+  const yearDateTime = new Date().toUTCString();
+  // console.log(yearDateTime);
+  
+
+  // CRUD
+  // Aquí se agrega un objeto a la coleccion "comments" del firestore
+  db.collection("comments").add({
+      dateTime: yearDateTime,
+      comment: texto
+    })
+    //luego, 
+    .then(function (docRef) {
+      console.log("Document written with ID: ", docRef.id);
+      text.value = '';
+    })
+    .catch(function (error) {
+      console.error("Error adding document: ", error);
+    });
+  })
+
+  let container = document.getElementById('container')
+// Aquí lee los documentos de la coleccion "comments" y el querySnapshot se repitirá por cada comment por el forEach
+//onSnapshot=agente de escucha--> actualización en tiempo real
+  db.collection("comments").onSnapshot((querySnapshot) => {
+    container.innerHTML = '';
+    querySnapshot.forEach((doc) => {
+        // console.log(`${doc.id} => ${doc.data().comment}`);
+
+// 
+
+    container.innerHTML += `<div class="card">
+                                  <div class="contenidoC">
+                                    <p>${doc.data().dateTime}</p>
+                                    <p>${doc.data().comment}</p>
+                                  </div>
+                                </div>`
+    });
+});
+
+
+
+
+// ASI LO TENIAMOS ANTES Y NO FUNCIONABA
 /*function comentario() {
     
 
@@ -28,47 +74,3 @@ console.log(text);
 
                 container.innerHTML = divPost;
 };*/
-
-boton.addEventListener("click", event => {
-//   console.log('holi');
-  let texto = text.value;
-//   console.log(texto);
-  let divPost = '';
-  divPost += `<div class="card">
-                      <div class="contenidoC">
-                          <p>${texto}</p>
-                      </div>
-                  </div>`
-//   console.log(divPost);
-  container.innerHTML = divPost;
-
-  // CRUD
-  db.collection("comments").add({
-      comment: texto
-    })
-    .then(function (docRef) {
-      console.log("Document written with ID: ", docRef.id);
-      text.value = '';
-    })
-    .catch(function (error) {
-      console.error("Error adding document: ", error);
-    });
-})
-
-// LEER DOCUMENTOS
-db.collection("comments").get().then((querySnapshot) => {
-    container.innerHTML = '';
-    querySnapshot.forEach((doc) => {
-        // console.log(`${doc.id} => ${doc.data()}`);
-        console.log(`${doc.id} => ${doc.data().comment}`);
-
-// CONVERTIR EN ARRAY? PARA TYPEOFARRAY() SORT()
-
-        container.innerHTML += `<div class="card">
-                      <div class="contenidoC">
-                          <p>${doc.data().comment}</p>
-                      </div>
-                  </div>`
-    });
-});
-
