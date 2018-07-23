2379ac6211ca3d04c8c6b5d5c679834be6d1d331
marelywoody <ros_ely_arc@hotmail.com>
2018-07-23 04:25:23 -0500

    facebook

--- src/js/authen.js
+++ src/js/authen.js
@@ -9,11 +9,24 @@
 // })
 let loginGoogle = document.getElementById('google-user');
 
-loginGoogle.addEventListener('click', event => {
-  let provider = new firebase.auth.GoogleAuthProvider();
-  provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+let provider = new firebase.auth.GoogleAuthProvider();
+
+loginGoogle.addEventListener('click', event => {  
   firebase.auth()
   .signInWithPopup(provider)
   .then(result => {
   })
-});    
+}); 
+
+let loginFacebook = document.getElementById('facebook-user');
+let providerFacebook = new firebase.auth.FacebookAuthProvider();
+
+loginFacebook.addEventListener('click', event => {
+  // firebase.auth().signInWithPopup(providerFacebook).then(function(result) {
+  //   // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+  //   var token = result.credential.accessToken;
+  //   // The signed-in user info.
+  //   var user = result.user;
+  // });
+  console.log('mndo');
+});
