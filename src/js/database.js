828e077be4892749b7e0bf94ceaf32131ef1f251
marelywoody <ros_ely_arc@hotmail.com>
2018-07-24 09:46:22 -0500

    separacion dom

--- src/js/database.js
+++ src/js/database.js
@@ -1,13 +1,11 @@
-// Accedo al servicio de la base de datos
-// let database = firebase.database();
-const wintowTras = () =>{
-  window.open('../index.html','_self');
-}
+// // Accedo al servicio de la base de datos
+// // let database = firebase.database();
+
 let close = document.getElementById('close');
 close.addEventListener('click', event => { 
   firebase.auth().signOut().then(function() {
     // Sign-out successful.
-    wintowTras();
+    window.open('../index.html','_self');
   }).catch(function(error) {
     // An error happened.
   });
--- src/js/index.js
+++ src/js/index.js
@@ -1,55 +1,29 @@
 let register = document.getElementById('register');
 let enter = document.getElementById('enter');
+let loginGoogle = document.getElementById('google-user');
+let loginFacebook = document.getElementById('facebook-user');
 
-const wintowTras = () =>{
-  window.open('../src/views/view1.html','_self');
-}
-// Evento donde se registran los usuarios código de firebase
 register.addEventListener('click', event => {
   let email = document.getElementById('email').value;
   let password = document.getElementById('password').value;
-  if (email != '' && password != '') {
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-    .catch(error => {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // ...
-    });
+  let userName = document.getElementById('user-name').value;
+  if (userName != '') {
+    authen.registerAccount(email,password); 
+  } else {
+    alert('Ingresa un nombre de usuario');
   }
 });
-// Evento de iniciar sesion
+
 enter.addEventListener('click', event => {
   let enterEmail = document.getElementById('enter-email').value;
   let enterPassword = document.getElementById('enter-password').value;
-  if (enterEmail != '' && enterPassword != '') {
-    firebase.auth().signInWithEmailAndPassword(enterEmail, enterPassword)
-    .catch(error => {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // ...
-    });
-  }
- });
-// funsión para ver si un usuario esta activo o no
-const monitor = () => {
-  firebase.auth().onAuthStateChanged(user => {
-    if (user) {
-      // User is signed in.
-      var displayName = user.displayName;
-      var email = user.email;
-      wintowTras();
-      var emailVerified = user.emailVerified;
-      var photoURL = user.photoURL;
-      var isAnonymous = user.isAnonymous;
-      var uid = user.uid;
-      var providerData = user.providerData;
-      // ...
-    } else {
-      // User is signed out.
-      // ...
-    }
-  });
-}; 
-monitor();
+  authen.loginAccount(enterEmail,enterPassword);
+});
+
+loginGoogle.addEventListener('click', event => {
+  authen.loginGoogle();
+});
+
+loginFacebook.addEventListener('click', event => {
+  authen.loginFacebook();
+});
