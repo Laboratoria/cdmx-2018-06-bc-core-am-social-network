2031966af0d9a98c9e6fff1f81660725dde915ad
adriana <adrivioleta@hotmail.com>
2018-07-23 19:07:12 -0500

    Acomodando funcionalidad de inicio de sesion con facebbok y google

--- src/js/app.js
+++ src/js/app.js
@@ -44,6 +44,31 @@ boton.addEventListener("click", event => {
   db.collection("comments").add({
       comment: texto
     })
+
+// Con esto se inicializa Nube de firestore a través de Firebase
+let db = firebase.firestore();
+
+
+let boton = document.getElementById('botonP');
+boton.addEventListener("click", event => {
+  // console.log('holi');
+  let text = document.getElementById('comentario');
+  // console.log(text);
+
+  let texto = text.value;
+//   construir nuevo objeto de fecha y convertirlo a un string UTC para pintarlo de manera estandard
+  const yearDateTime = new Date().toUTCString();
+  // console.log(yearDateTime);
+  
+
+  // CRUD
+  // Aquí se agrega un objeto a la coleccion "comments" del firestore
+  db.collection("comments").add({
+      dateTime: yearDateTime,
+      comment: texto
+    })
+    //luego, 
+
     .then(function (docRef) {
       console.log("Document written with ID: ", docRef.id);
       text.value = '';
@@ -51,6 +76,7 @@ boton.addEventListener("click", event => {
     .catch(function (error) {
       console.error("Error adding document: ", error);
     });
+
 })
 
 // LEER DOCUMENTOS
@@ -69,3 +95,42 @@ db.collection("comments").get().then((querySnapshot) => {
                   </div>`
     });
 });
+
+  })
+
+  let container = document.getElementById('container')
+// Aquí lee los documentos de la coleccion "comments" y el querySnapshot se repitirá por cada comment por el forEach
+//onSnapshot=agente de escucha--> actualización en tiempo real
+  db.collection("comments").onSnapshot((querySnapshot) => {
+    container.innerHTML = '';
+    querySnapshot.forEach((doc) => {
+        // console.log(`${doc.id} => ${doc.data().comment}`);
+
+    container.innerHTML += `<div class="card">
+                                  <div class="contenidoC">
+                                    <p>${doc.data().dateTime}</p>
+                                    <p>${doc.data().comment}</p>
+                                  </div>
+                                </div>`
+    });
+});
+
+
+
+
+// ASI LO TENIAMOS ANTES Y NO FUNCIONABA
+/*function comentario() {
+    
+
+    let divPost = '';
+    let comment = text.value;
+    console.log(comment);
+      divPost += `<div class="card">
+                    <div class="contenidoC">
+                        <p>${comment}</p>
+                    </div>
+                </div>`
+
+                container.innerHTML = divPost;
+};*/
+
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,1 +1,20 @@
 // Ejecuta tu test 
+
+// Ejecuta tu test 
+// 1.Start by writing a test
+// 2.Run the test and any other tests. At this point, your newly added test should fail. If it doesn’t fail here, it might not be testing the right thing and thus has a bug in it.
+// 3.Write the minimum amount of code required to make the test pass
+// 4.Run the tests to check the new test passes
+// 5.Optionally refactor your code
+// 6.Repeat from 1
+
+
+
+
+//AQUI VAN LOS ITS Y ASSERTS
+describe('registrar()', () => {
+
+  it('debería ser una función', () => {
+    assert.equal(typeof 'registrar()', 'function');
+  });
+});
