3771eae2dd52af8393f75406c40434bb98583737
alppacat <lorena.pratts@gmail.com>
2018-07-24 12:49:07 -0500

    Actualiza foto de perfil por usuario logueado en la web

--- src/js/home.js
+++ src/js/home.js
@@ -20,53 +20,28 @@
         displayName: user.displayName
       });
       document.getElementById('user-paragraph').innerHTML = `Bienvenidx ${user.displayName}`;
+      document.getElementById('profile-image').innerHTML = `<img src="${user.photoURL}">`;
+      console.log(user.photoURL);
     } else {
       console.log('not logged in');
     }
     let id = user.uid;
     userConect = database.ref('users/' + id);
-    addUser(user.displayName, user.email);
+    addUser(user.displayName, user.email, user.photoURL);
   });
 
-<<<<<<< HEAD
-  function agregarUser(uid, name, email) {
-    let user = firebase.auth().currentUser;
-
-    var conectados = userConect.push({
-      uid: uid,
-      name: name,
-      email: email
-    });
-  }
-=======
-  addUser = (name, email) => {
+  addUser = (name, email, photo) => {
     let conect = userConect.push({
       name: name,
-      email: email
+      email: email,
+      photo: photo
     });
   };
 }());
->>>>>>> cd36d52b5e1f79137d1b8941c7bcc8ee162a0a06
 
-  const postText = document.getElementById('post-entry');
-  const btnShare = document.getElementById('new-post');
+const postText = document.getElementById('post-entry');
+const btnShare = document.getElementById('new-post');
 
-  btnShare.addEventListener('click', event => {
-    const currentUser = firebase.auth().currentUser;
-    const textInPost = postText.value;
-    // Create a unique key for messages collection
-    const newPostKey = firebase.database().ref().child('posts').push().key;
-    firebase.database().ref(`posts/${newPostKey}`).set({
-      creator: currentUser.uid,
-      creatorName: currentUser.displayName,
-      text: textInPost
-    });
-    window.social.displayPost(textInPost);
-  });
-}());
-
-<<<<<<< HEAD
-=======
 btnShare.addEventListener('click', event => {
   const currentUser = firebase.auth().currentUser;
   const textInPost = postText.value;
@@ -86,4 +61,3 @@ btnShare.addEventListener('click', event => {
   };
   // window.social.displayPost(textInPost);
 });
->>>>>>> cd36d52b5e1f79137d1b8941c7bcc8ee162a0a06
