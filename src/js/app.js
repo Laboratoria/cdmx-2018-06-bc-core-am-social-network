424dd7c5809f54e0c674220ed0c41e7a3ec364c9
adriana <adrivioleta@hotmail.com>
2018-07-24 13:46:38 -0500

    login actualizacion

--- src/js/app.js
+++ src/js/app.js
@@ -5,119 +5,82 @@ firebase.initializeApp({
   projectId: "red-social-237f9"
 });
 
-// Initialize Cloud Firestore through Firebase
-var db = firebase.firestore();
-
-
-let text = document.getElementById('comentario');
-let boton = document.getElementById('botonP');
-let newPost = document.getElementById('container');
-console.log(text);
-
-/*function comentario() {
-
-    let divPost = '';
-    let comment = text.value;
-    console.log(comment);
-      divPost += `<div class="card">
-                    <div class="contenidoC">
-                        <p>${comment}</p>
-                    </div>
-                </div>`
-                container.innerHTML = divPost;
-};*/
-
-boton.addEventListener("click", event => {
-//   console.log('holi');
-  let texto = text.value;
-//   console.log(texto);
-  let divPost = '';
-  divPost += `<div class="card">
-                      <div class="contenidoC">
-                          <p>${texto}</p>
-                      </div>
-                  </div>`
-//   console.log(divPost);
-  container.innerHTML = divPost;
-
-  // CRUD
-  db.collection("comments").add({
-      comment: texto
-    })
-
 // Con esto se inicializa Nube de firestore a través de Firebase
 let db = firebase.firestore();
 
-
-let boton = document.getElementById('botonP');
+const boton = document.getElementById('botonP');
 boton.addEventListener("click", event => {
   // console.log('holi');
   let text = document.getElementById('comentario');
   // console.log(text);
 
   let texto = text.value;
-//   construir nuevo objeto de fecha y convertirlo a un string UTC para pintarlo de manera estandard
+  //   construir nuevo objeto de fecha y convertirlo a un string UTC para pintarlo de manera estandard
   const yearDateTime = new Date().toUTCString();
   // console.log(yearDateTime);
 
 
   // CRUD
   // Aquí se agrega un objeto a la coleccion "comments" del firestore
+  // el "add" agrega un id único en automatico
   db.collection("comments").add({
       dateTime: yearDateTime,
       comment: texto
     })
+<<<<<<< HEAD
     //luego,
 
+=======
+    //luego, 
+>>>>>>> upstream/master
     .then(function (docRef) {
       console.log("Document written with ID: ", docRef.id);
+      // con esto se reinician los input despues con el "click", o sea, una vez que se haya guardado el dato, va a generar un string vacio
       text.value = '';
     })
     .catch(function (error) {
       console.error("Error adding document: ", error);
     });
-
 })
 
-// LEER DOCUMENTOS
-db.collection("comments").get().then((querySnapshot) => {
-    container.innerHTML = '';
-    querySnapshot.forEach((doc) => {
-        // console.log(`${doc.id} => ${doc.data()}`);
-        console.log(`${doc.id} => ${doc.data().comment}`);
-
-// CONVERTIR EN ARRAY? PARA TYPEOFARRAY() SORT()
-
-        container.innerHTML += `<div class="card">
-                      <div class="contenidoC">
-                          <p>${doc.data().comment}</p>
-                      </div>
-                  </div>`
-    });
-});
-
-  })
-
-  let container = document.getElementById('container')
+let container = document.getElementById('container')
 // Aquí lee los documentos de la coleccion "comments" y el querySnapshot se repitirá por cada comment por el forEach
 //onSnapshot=agente de escucha--> actualización en tiempo real
-  db.collection("comments").onSnapshot((querySnapshot) => {
-    container.innerHTML = '';
-    querySnapshot.forEach((doc) => {
-        // console.log(`${doc.id} => ${doc.data().comment}`);
+db.collection("comments").onSnapshot((querySnapshot) => {
+  container.innerHTML = '';
+  querySnapshot.forEach((doc) => {
+    // console.log(`${doc.id} => ${doc.data().comment}`);
+
+    // 
 
     container.innerHTML += `<div class="card">
                                   <div class="contenidoC">
                                     <p>${doc.data().dateTime}</p>
                                     <p>${doc.data().comment}</p>
+                                    <button class="btn"><i class="fas fa-gem"></i></button>
+                                    <button class="btn"><i class="fas fa-share"></i></button>
+                                    <button class="btn" onclick="edit('${doc.data().comment}')"><i class="fas fa-edit"></i></button>
+                                    <button class="btn"><i class="fas fa-save"></i></button>
+                                    <button class="btn" onclick="eliminate('${doc.id}')"><i class="fas fa-trash-alt"></i></button>
                                   </div>
                                 </div>`
-    });
+  });
 });
 
 
+// Para borrar documentos de la coleccion en tiempo real:
+function eliminate(id) {
+  db.collection("comments").doc(id).delete().then(function () {
+    console.log("Document successfully deleted!");
+  }).catch(function (error) {
+    console.error("Error removing document: ", error);
+  });
+}
 
+// Para editar documentos de la coleccion en tiempo real:
+function edit(texto) {
 
+<<<<<<< HEAD
 // ASI LO TENIAMOS ANTES Y NO FUNCIONABA
 /*function comentario() {
 
@@ -133,4 +96,19 @@ db.collection("comments").get().then((querySnapshot) => {
 
                 container.innerHTML = divPost;
 };*/
+=======
+  document.getElementById('comentario').value = texto
+>>>>>>> upstream/master
 
+  let inputRef = db.collection("comments").doc(id);
+  return inputRef.update({
+      comment: texto
+    })
+    .then(function () {
+      console.log("Document successfully updated!");
+    })
+    .catch(function (error) {
+      // The document probably doesn't exist.
+      console.error("Error updating document: ", error);
+    });
+}
