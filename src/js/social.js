3377a67d0aeaf53117da4f1da55b0962c4459bef
lameme <esme.riveroh@gmail.com>
2018-07-24 20:54:31 -0500

    Agregando autenticación de correo y contraseña

--- src/js/social.js
+++ src/js/social.js
@@ -1,5 +1,40 @@
-// window.onload = initialization;
+window.onload = initialization;
+let formSignup;
+let refUsers;
+let refUserAuth;
 
-// initialization = () => {
-//   let database = firebase.database();
-// };
+function initialization() {
+  const config = {
+    apiKey: 'AIzaSyCjN9x4Q4B8Nx5xf1ZoKLpWn4mTPiuuC3c',
+    authDomain: 'red-social-19985.firebaseapp.com',
+    databaseURL: 'https://red-social-19985.firebaseio.com',
+    projectId: 'red-social-19985',
+    storageBucket: 'red-social-19985.appspot.com',
+    messagingSenderId: '169924096887'
+  };
+      
+  firebase.initializeApp(config);
+
+  formSignup = document.getElementById('form-signup'); // Hace referencia al formulario
+  formSignup.addEventListener('submit', sendSignup, false); // Se crea el evento del boton submit del formulario
+  refUsers = firebase.database().ref().child('users'); // Se hace referencia a la rama de la bd en la que almacenaremos la info
+  refUserAuth = firebase.auth();
+}
+
+function sendSignup(event) { // En esta función se agrega los datos del formulario en la bd
+  event.preventDefault();
+  refUsers.push({
+    ciudad: event.target.ciudad.value,
+    correo: event.target.correo.value,
+    edad: event.target.edad.value,
+    nombre: event.target.nombre.value,
+    pais: event.target.pais.value,
+    password: event.target.password.value,
+  });
+  if (refUserAuth.createUserWithEmailAndPassword(event.target.correo.value, event.target.password.value)) {
+    alert(event.target.nombre.value + ' quedaste registradx');
+  }
+  else {
+    alert('Confirma que hayas escrito tus datos caorrectamente!');
+  }
+}
