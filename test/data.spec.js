6b7cdb2121f1ef9774f9a13405e7be4c927208d1
SakuraBravo <sakura.bravo@gmail.com>
2018-07-25 08:02:02 -0500

    Agrega icono.jpeg a la pestaña de todas las ventanas

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,1 +1,25 @@
-// Ejecuta tu test 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Ejecuta tu test
+describe('index', () => { // es el data.js
+
+  it('debería ser una función', () => {
+    assert.equal(typeof authGoogle, 'function');
+  });
--- test/headless.js
+++ test/headless.js
@@ -1,1 +1,30 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Configura tus test para usarlos
+global.window = global;
+global.assert = require('chai').assert;
+global.fixtures = {
+  users: require('../data/firebase.json'),
+};
+require('../src/index');
+require('./data.spec.js');
