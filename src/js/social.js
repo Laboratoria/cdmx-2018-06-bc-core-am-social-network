61c15d1908e0201cb5b279446a7c89939c9774a3
lameme <esme.riveroh@gmail.com>
2018-07-30 10:40:42 -0500

    Modificando el timeline

--- src/js/social.js
+++ src/js/social.js
@@ -33,24 +33,7 @@ function sendSignup(event) { // En esta función se agrega los datos del formula
   });
   if (refUserAuth.createUserWithEmailAndPassword(event.target.correo.value, event.target.password.value)) {
     alert(event.target.nombre.value + ' quedaste registradx');
-  }
-  else {
-<<<<<<< HEAD
+  } else {
     alert('Confirma que hayas escrito tus datos correctamente!');
   }
 }
-=======
-    alert('Confirma que hayas escrito tus datos caorrectamente!');
-  }
-}
-
-function authentication(event) {
-  event.preventDefault();
-  refUserAuth.signInWithEmailAndPassword(event.target.correo.value, event.target.password.value).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-  });
-}
->>>>>>> upstream/master
