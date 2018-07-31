ecf630a5f2adbbbf37d23a0cb0eeae7322d83553
unknown <feerkay15@gmail.com>
2018-07-31 08:20:09 -0500

    se agrega bien el enlace al muro al iniciar sesion

--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,7 @@ loginGoogle: () => {
     const provider = new firebase.auth.GoogleAuthProvider();
     provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
     firebase.auth().signInWithPopup(provider).then(result => {
-     location.href = ("../views/wall.html");
+     location.href = ("../src/views/wall.html");
    })
     .catch (e => alert(e.message));
     } else {
@@ -22,7 +22,7 @@ if (!firebase.auth().currentUser) {
       const provider = new firebase.auth.FacebookAuthProvider();
       provider.addScope('public_profile');
       firebase.auth().signInWithPopup(provider).then(result => {
-        location.href = ("../views/wall.html");
+        location.href = ("../src/views/wall.html");
       })
       .catch (e => alert(e.message));
     } else {
@@ -35,7 +35,7 @@ if (!firebase.auth().currentUser) {
     const provider = new firebase.auth.GithubAuthProvider();
     provider.addScope('repo');
     firebase.auth().signInWithPopup(provider).then(result => {
-      location.href = ("../views/wall.html");
+      location.href = ("../src/views/wall.html");
     })
     .catch(e => alert(e.message));
     } else {
--- test/headless.js
+++ test/headless.js
@@ -1,2 +1,5 @@
 // Configura tus test para usarlos
-
+global.window = global;
+global.assert = require('chai').assert;
+require('../src/login');
+require('./data.spec.js');
