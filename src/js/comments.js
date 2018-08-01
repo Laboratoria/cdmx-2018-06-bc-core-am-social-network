0bd72189babdeb8f4b1f839cf4063d0b91c8ab20
zareth <zareth_abarca@hotmail.com>
2018-08-01 01:59:27 -0500

    resolviendo conflictos

--- src/js/comments.js
+++
@@ -1,7 +0,0 @@
-
-// Contador de likes
-let countLikes = 0;
-const addLike = () => { /* Cuando sucede una llamada al evento del boton like, se acumula un punto que se mostrara al lado */
-  countLikes = countLikes + 1;
-  document.getElementById('likeCounter').textContent = countLikes;
-};
