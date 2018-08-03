02adc12031930ade852aab060e59fbf009090ad7
gloryarz <gloryarz@gmail.com>
2018-08-02 20:14:29 -0500

    Se agregan comentarios

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,11 +1,11 @@
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyCGT48tRRTrbUJvfWb3PvKZfSgISreF1S0",
-  authDomain: "red-social-96a75.firebaseapp.com",
-  databaseURL: "https://red-social-96a75.firebaseio.com",
-  projectId: "red-social-96a75",
-  storageBucket: "red-social-96a75.appspot.com",
-  messagingSenderId: "775319261849"
+  apiKey: 'AIzaSyCGT48tRRTrbUJvfWb3PvKZfSgISreF1S0',
+  authDomain: 'red-social-96a75.firebaseapp.com',
+  databaseURL: 'https://red-social-96a75.firebaseio.com',
+  projectId: 'red-social-96a75',
+  storageBucket: 'red-social-96a75.appspot.com',
+  messagingSenderId: '775319261849'
 };
 firebase.initializeApp(config);
 let database = firebase.database();
